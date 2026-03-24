e82b2c3ca03dce17c85aa5f84f2eed2d08ca04c9
pardo133 <pardo133@hotmail.com>
2026-03-24 15:21:45 +0100

    m

---
+++ multiplicar.js
@@ -0,0 +1,14 @@
+const btnMulti = document.getElementById("btn-multi");
+
+btnMulti.onclick = () => {
+    const n1 = Number(document.getElementById("num1").value);
+    const n2 = Number(document.getElementById("num2").value);
+
+    operacion.valorA = n1;
+    operacion.valorB = n2;
+    operacion.tipo = "multiplicación";
+    operacion.total = n1 * n2;
+
+    document.getElementById("resultado").textContent = `Resultado: ${operacion.total}`;
+    historial.push({ ...operacion });
+};
